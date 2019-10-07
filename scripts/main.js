6c55ceadf401d029101fee2c577c13bc760f32fa
max-school <35243264+sonofkoolaidman@users.noreply.github.com>
2019-10-07 09:03:12 -0500

    began eventlistener work

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
 // Put your JavaScript here
 const REST_IMAGE_SELECTOR = '[data-image-role="target"]'
+const HIDDEN_PROPERTY_CLASS = 'hidden-prop'
+
+function moveRest() {
+  'use strict'
+  document.querySelector(REST_IMAGE_SELECTOR).classList.add(HIDDEN_PROPERTY_CLASS);
+}
+
+function addRestClickHandler(thumb) {
+  thumb.addEventListener('click', function(event) {
+    event.preventDefault();
+    moveRest();
+  })
+}
