8edac28db93bcc44c0abe0679933d90225c9c2b5
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-10-07 20:10:30 -0500

    started image moving system.

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,8 @@
 const REST_IMAGE_SELECTOR = '[data-image-role="target"]'
 const HIDDEN_PROPERTY_CLASS = 'hidden-prop'
 
+
 function moveRest() {
   'use strict'
-  document.querySelector(REST_IMAGE_SELECTOR).classList.add(HIDDEN_PROPERTY_CLASS);
-}
-
-function addRestClickHandler(thumb) {
-  thumb.addEventListener('click', function(event) {
-    event.preventDefault();
-    moveRest();
-  })
+  let detailImage = document.querySelector(REST_IMAGE_SELECTOR)
 }
