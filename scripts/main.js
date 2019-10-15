752b533771bd1cd81543c3d810885b8ec74b81bc
max-school <35243264+sonofkoolaidman@users.noreply.github.com>
2019-10-15 09:07:07 -0500

    worked on javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 // Put your JavaScript here
 const REST_IMAGE_SELECTOR = '[data-image-role="target"]'
 const HIDDEN_PROPERTY_CLASS = 'hidden-prop'
+const FOOD_IMAGE_SELECTOR = '[data-image-role="trigger"]'
 
-
-function moveRest() {
-  'use strict'
-  let detailImage = document.querySelector(REST_IMAGE_SELECTOR)
+function setDetails(imageUrl) {
+  'use strict';
+  let restImage = document.querySelector(REST_IMAGE_SELECTOR);
+  restImage.setAttribute('src', imageUrl);
 }
