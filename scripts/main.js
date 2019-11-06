90a6afc7ec697efc067a79f22c1629bb7f5ff461
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-11-06 16:52:35 -0600

    finished

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,34 @@ function setDetails(imageUrl) {
 }
 
 function imageFromThumb(thumbnail) {
-  'use strict'
+  'use strict';
   return thumbnail.getAttribute('data-image-url');
 }
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+  'use strict';
+  thumb.addEventListener('click', function(event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+  });
+}
+
+function getThumbnailsArray() {
+  'use strict';
+  let thumbnails = document.querySelectorAll(FOOD_IMAGE_SELECTOR);
+  let thumbnailArray = [].slice.call(thumbnails); //convert the NodeList to an array
+  return thumbnailArray;
+}
+
+function initializeEvents() {
+  'use strict';
+  let thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
